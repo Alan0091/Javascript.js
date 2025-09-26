9003eee89585d574fe09941dfe4e92ea58634bad
Alan0091 <105783204+Alan0091@users.noreply.github.com>
2025-09-26 19:04:47 +0000

    conversion of datatype

---
+++ chai01/02babu.js
@@ -0,0 +1,18 @@
+"use stict" //this is java scipt New version its change old java code to new versions of code.
+
+// alert(3+3); We're using nodel.js not browser.
+
+// let score = "33";
+
+//console.log(typeof score);
+
+// "33"  String
+// 33 number
+// "33abc" Nan
+
+let somenumber = 30
+
+let Stringnumber = String(somenumber)
+
+console.log(Stringnumber);
+console.log(typeof Stringnumber);
