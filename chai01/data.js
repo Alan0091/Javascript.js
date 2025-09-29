73d34f5bc547f72d6507ce436eef7a660b9b0d86
Alan0091 <105783204+Alan0091@users.noreply.github.com>
2025-09-29 14:30:36 +0000

    datatypes

---
+++ chai01/data.js
@@ -0,0 +1,42 @@
+//Primitive datatype
+
+// 7 types = string, number, boolean, null, undefined, symbol, bigint.
+
+//const ID = 100;
+//console.log(ID);
+
+//const accountNumber = 2*222;
+//console.log(accountNumber);
+
+//let isloggedin = Boolean(true);
+//console.log(isloggedin);
+
+//let name = null;
+//console.log(name);
+
+//let currency = Symbol("button");
+//console.log(currency);
+
+//let country = 9547030054053404n
+//console.log(country);
+
+console.log(typeof currency);
+
+
+// Reference is also called Non primitive datatype - array, objects, function.
+
+let movie = ["Avenger", "Conjuring", "It"];
+
+//let objects = {
+
+   // name :"govind",
+    //age: 22,
+  //  sex:"M"
+
+//}
+
+/*let myfunction = function(){
+    console.log("hello govind");
+}*/
+
+console.log(typeof myfunction);
